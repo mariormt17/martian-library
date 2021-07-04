c2c2a87876b715a23329a39a5dcbfabaeaf1e998
Mario Mancilla <mariormt17@gmail.com>
2021-07-03 20:49:25 -0500

    Allow use of action cable to apollo file

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -1,11 +1,15 @@
-// Client
+// client
 import { ApolloClient } from 'apollo-client';
-// Cache
+// cache
 import { InMemoryCache } from 'apollo-cache-inmemory';
-// Links
+// links
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink, Observable } from 'apollo-link';
+
+import ActionCable from 'actioncable';
+import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
+
 export const createCache = () => {
   const cache = new InMemoryCache();
   if (process.env.NODE_ENV === 'development') {
@@ -14,7 +18,26 @@ export const createCache = () => {
   return cache;
 };
 
-// getToken from meta tags
+const getCableUrl = () => {
+  const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+  const host = window.location.hostname;
+  const port = process.env.CABLE_PORT || '3000';
+  const authToken = localStorage.getItem('mlToken');
+  return `${protocol}//${host}:${port}/cable?token=${authToken}`;
+};
+
+const createActionCableLink = () => {
+  const cable = ActionCable.createConsumer(getCableUrl());
+  return new ActionCableLink({ cable });
+};
+
+const hasSubscriptionOperation = ({ query: { definitions } }) =>
+  definitions.some(
+    ({ kind, operation }) =>
+      kind === 'OperationDefinition' && operation === 'subscription'
+  );
+
+
 const getTokens = () => {
   const tokens = {
     'X-CSRF-Token': document
@@ -56,33 +79,38 @@ const createLinkWithToken = () =>
   );
 
 // log erors
-const logError = (error) => console.error(error);
+const logError = error => console.error(error);
 // create error link
-const createErrorLink = () => onError(({ graphQLErrors, networkError, operation }) => {
-  if (graphQLErrors) {
-    logError('GraphQL - Error', {
-      errors: graphQLErrors,
-      operationName: operation.operationName,
-      variables: operation.variables,
-    });
-  }
-  if (networkError) {
-    logError('GraphQL - NetworkError', networkError);
-  }
-})
+const createErrorLink = () =>
+  onError(({ graphQLErrors, networkError, operation }) => {
+    if (graphQLErrors) {
+      logError('GraphQL - Error', {
+        errors: graphQLErrors,
+        operationName: operation.operationName,
+        variables: operation.variables,
+      });
+    }
+    if (networkError) {
+      logError('GraphQL - NetworkError', networkError);
+    }
+  });
 
-// http link
-const createHttpLink = () => new HttpLink({
-  uri: '/graphql',
-  credentials: 'include',
-})
+const createHttpLink = () =>
+  new HttpLink({
+    uri: '/graphql',
+    credentials: 'include',
+  });
 
 export const createClient = (cache, requestLink) => {
   return new ApolloClient({
     link: ApolloLink.from([
       createErrorLink(),
       createLinkWithToken(),
-      createHttpLink(),
+      ApolloLink.split(
+        hasSubscriptionOperation,
+        createActionCableLink(),
+        createHttpLink(),
+      ),
     ]),
     cache,
   });
